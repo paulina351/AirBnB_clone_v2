b014094860c7a6514231f5515750bb06116a03ea
Paulina Udeh <udehpaulina93@gmail.com>
2023-11-22 23:31:35 +0000

    AirBnB

--- console.py
+++ console.py
@@ -2,6 +2,8 @@
 """ Console Module """
 import cmd
 import sys
+from shlex import split
+from datetime import datetime
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
@@ -115,16 +117,34 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
-            print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+        try:
+            if not args:
+                raise SyntaxError()
+            my_list = line.split(" ")
+
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '"':
+                    value = valye.strip('"').replace("_", " ")
+                else:
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                    kwargs[key] = value
+                if kwargs == {}:
+                    obj = eval(my_list[0])()
+                else:
+                    obj = eval(my_list[0])(**kwargs)
+                    storage.new(obj)
+                print(obj.id)
+                obj.save()
+
+            except SyntaxError:
+                 print("** class name missing **")
+            except NameError:
+                 print("** class doesn't exist **")
 
     def help_create(self):
         """ Help information for the create method """
--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
+from os import getenv
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from models.city import City
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related cities."""
+            city_1 = []
+            for city in list(models.storage.all(City).values()):
+                if city.state.id == self.id:
+                    city_1.append(city)
+                return city_1
