c9c6bb90ac68974202a9bcbcbe2e04533f4ffc62
Paulina Udeh <udehpaulina93@gmail.com>
2024-01-01 22:46:19 +0000

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
+"""A  script that starts a Flask web application
+   /c/<text>: display “C ” followed by the value of 
+   the text variable (replace underscore _ symbols
+   with a space )
 """
+
+
 from flask import Flask
+from markupsafe import escape
+import re
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+def hello_route():
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Displays 'HBNB'."""
+def hbnb_route():
     return "HBNB"
 
+@app.route("/c/is_fun", strict_slashes=False)
+def c_route():
+    return "C is fun"
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>."""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+
+@app.route("/c/<cool>", strict_slashes=False)
+def c_route_2(cool):
+    return f"C {escape(cool)}"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(debug=True, port=5000, host="0.0.0.0")
